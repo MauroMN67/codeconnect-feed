2f46b7f77236a189b00ec83f3daf8638d2b60d48
MAURO MONTE NASCIMENTO <mauro.nascimento@tjsp.jus.br>
2024-10-04 18:50:33 -0300

    refazendo config gh-pages

--- src/componentes/Ordenacao/index.jsx
+++ src/componentes/Ordenacao/index.jsx
@@ -1,19 +1,13 @@
-import "./styles.css";
+import './styles.css'
 export default function Ordenacao() {
-  return (
-    <section className="container-ordenacao">
-      <ul>
-        <li>
-          <a href="#" className="item item-selecionado">
-            Recentes
-          </a>
-        </li>
-        <li>
-          <a href="#" className="item">
-            Antigos
-          </a>
-        </li>
-      </ul>
-    </section>
-  );
+    return (
+        <ul className="lista-ordenacao">
+            <li>
+                <a href='' className='lista-ordenacao__link lista-ordenacao__link--ativo'>Recentes</a>
+            </li>
+            <li>
+                <a href='' className='lista-ordenacao__link'>Recentes</a>
+            </li>
+        </ul>
+    )
 }
